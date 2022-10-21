01f4405c480ee4dc06d21521f923e694d3e83fc8
LHTrungSkySP <lehoangtrung038582@gmail.com>
2022-10-21 16:45:16 +0700

    lần 1

---
+++ GĐ1/js/main.js
@@ -0,0 +1,21 @@
+// Author: Lê hoàng trung 
+$(document).ready(function () {
+    intitEvent();
+    // kiểm tra target
+    // $(document).click(function(evt){
+    //     debugger
+    // })
+})
+
+function intitEvent(){
+    $('#ticketModal').show();
+    
+}
+
+function showModal(){
+    $('#ticketModal').show();
+}
+
+function closeModal(){
+    $('#ticketModal').hide();
+}
