f394c591484b8f7fe91bbbad8be243e434a71226
LHTrungSkySP <lehoangtrung038582@gmail.com>
2022-11-22 13:59:08 +0700

    layout 1

---
+++ Proj01/UI_Elearning/UI_Elearning/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace UI_Elearning.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
