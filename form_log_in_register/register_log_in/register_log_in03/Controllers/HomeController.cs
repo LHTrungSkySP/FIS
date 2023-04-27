2365c2235df79cee03366ac7ca51ccec0ae6d4b9
LHTrungSkySP <lehoangtrung038582@gmail.com>
2023-04-28 00:12:18 +0700

    ok

---
+++ form_log_in_register/register_log_in/register_log_in03/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace register_log_in03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ form_log_in_register/register_log_in/register_log_in03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="register_log_in03.MvcApplication" Language="C#" %>
